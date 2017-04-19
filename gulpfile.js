266f2bb194fc2057441c521f85ab61af19a25f46
philipalexander <pjalexander2@gmail.com>
2017-04-18 23:31:20 -0600

    simplified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,16 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var cssnano = require('gulp-cssnano');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
+// var cssnano = require('gulp-cssnano');
+// var sourcemaps = require('gulp-sourcemaps');
+// var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function () {
-  gulp.watch('./src/**/*.scss', ['workflow']);
+  gulp.watch('./src/**/*.scss', ['sass']);
 });
 
-gulp.task('workflow', function () {
-  gulp.src('./src/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(cssnano())
-    .pipe(sourcemaps.write('./'))
-  .pipe(gulp.dest('./dist/css/'))
+gulp.task('sass', function () {
+    return gulp.src('./src/**/*.scss')
+      .pipe(sass.sync().on('error', sass.logError))
+      .pipe(gulp.dest('./src'));
 });
