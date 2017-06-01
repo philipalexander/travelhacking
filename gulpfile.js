e7764bdc50cf38f72f79f3e7f24206bd12da34df
philipalexander <pjalexander2@gmail.com>
2017-06-01 08:37:47 -0600

    added gulp serve task, added cookie tracking js, moved index.html to root

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,39 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var connect = require('gulp-connect');
 // var cssnano = require('gulp-cssnano');
 // var sourcemaps = require('gulp-sourcemaps');
 // var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function () {
-  gulp.watch('./src/**/*.scss', ['sass']);
-});
+
+
+gulp.task('default', ['sass', 'serve', 'watch']);
 
 gulp.task('sass', function () {
     return gulp.src('./src/**/*.scss')
       .pipe(sass.sync().on('error', sass.logError))
-      .pipe(gulp.dest('./src'));
+      .pipe(gulp.dest('./src'))
+      .pipe(browserSync.reload({
+          stream: true
+      }));
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+  })
+});
+
+gulp.task('serve', function() {
+  connect.server();
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./src/**/*.scss', ['sass']);
+  gulp.watch('./src/*.html', browserSync.reload);
+  gulp.watch('./src/**/*.js', browserSync.reload);
 });
---
+++ src/referralTracking.js
@@ -0,0 +1,39 @@
+(function() {
+    console.log("checkmate");
+
+    createCookie('referrer','philip',7);
+    
+    var x = readCookie('referrer')
+    if (x) {
+        console.log("cookie set already");
+    }
+
+})();
+
+
+function createCookie(name,value,days) {
+    console.log("in createCookie");
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    console.log(name + "=" + value + expires + "; path=/");
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
